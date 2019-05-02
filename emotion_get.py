a5917b26622a2c1e28e7065055ed7b1011e0ebf6
WEICHI <30483824+apou12202@users.noreply.github.com>
2019-05-03 01:07:37 +0800

    get emotion(angry&love)

--- emotion_get.py
+++ emotion_get.py
@@ -4,14 +4,11 @@ import angry_get
 import io
 import time
 
-url = "https://www.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier=ZmVlZGJhY2s6NjE1NjA5NDQ4OTEyMzMx&av=100001602414974" # 1
-# url = "https://www.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier=ZmVlZGJhY2s6MjYzMzI4MDUxMzM1NTYxOA%3D%3D&av=100001602414974" # 2
-#url = "https://www.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier=ZmVlZGJhY2s6MjYzNzk4MjU3NjIxODc0NQ%3D%3D&av=100001602414974" # 3
+url = "https://www.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier=ZmVlZGJhY2s6MjY0NDQ2MzQwODkwMzk5NQ%3D%3D&av=100001602414974"
 
 angry = angry_get.main(url)
-time.sleep(0.1)
-love = love_get.main(url)
 
+love = love_get.main(url)
 
 
 task = [{
